3173eefa882ee7e1241cb16327a3fbe0d8cf7adc
Alan Cohen & Justin Lee <pair+acjetnut.leeju@devbootcamp.com>
2014-09-04 12:08:14 -0400

    Add validations to Document migration

--- db/migrate/20140904141324_create_documents.rb
+++ db/migrate/20140904141324_create_documents.rb
@@ -1,8 +1,8 @@
 class CreateDocuments < ActiveRecord::Migration
   def change
     create_table :documents do |t|
-      t.belongs_to :user
-      t.string :title
+      t.belongs_to :user, null: false
+      t.string :title, null: false
       t.text :context
       t.string :file_extension
 
