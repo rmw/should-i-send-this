792910aada1775e1d10e3f9d70055ab61eb08878
Alan Cohen <awc583@gmail.com>
2014-09-06 10:19:22 -0400

    added name to user input

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
 
-  devise_for :users
+  devise_for :users, :controllers => { registrations: 'registrations' }
+
+
   root :to => "public#index"
 
   get '/styleguide' => 'public#show'
