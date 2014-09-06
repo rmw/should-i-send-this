2913f42848e16d706f9c5b6878ae27b3c6f6dbf5
Avi Fox-Rosen <frosenox@gmail.com>
2014-09-06 18:50:00 -0400

    first feature test user creates doc navs to profile page, passes

--- spec/features/user_signs_up_creates_visits_profile_page.rb
+++ spec/features/user_signs_up_creates_visits_profile_page.rb
@@ -1,9 +1,7 @@
 require 'rails_helper'
 
 
-feature "User Log In Create Doc Versions and Navigate to Profile page" do
-
-
+feature "User Logs In Create Doc Versions and Navigate to Profile page" do
 
   scenario "User navigates to new user form, inputs valid version info and saves" do
     visit root_url
@@ -33,24 +31,9 @@ feature "User Log In Create Doc Versions and Navigate to Profile page" do
     expect(page).to have_content 'valid content'
 
     click_link 'Test Name'
-    # expect(page).to have_content 'What Type of Feedback Are You Looking For?'
+    expect(current_url).to eq "http://www.example.com/users/#{User.last.id}"
 
-    # fill_in 'Title', :with => 'valid title'
-    # fill_in 'Context', :with => 'valid context'
-    # fill_in 'Content', :with => 'valid content'
-
-    # check('Positivity') # check box button from feedback option sidebar
-
-    # click_button 'Submit'
-
-    # expect(current_url).to eq "www.example.com/documents/#{Document.first.id}"
-    # expect(page).to have_content 'valid title'
-    # expect(page).to have_content 'valid context'
-    # expect(page).to have_content 'valid content'
-
-    # expect(page).to have_content 'Positivity Results:'
-
+    expect(page).to have_content 'valid title'
+    expect(page).to have_content 'valid context'
   end
-
-
 end
