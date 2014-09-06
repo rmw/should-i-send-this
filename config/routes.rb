d6eac1799c284ee1263ba19833002a7eac3d33c8
Avi Fox-Rosen <frosenox@gmail.com>
2014-09-06 14:12:11 -0400

    add user show, for personal profile

--- config/routes.rb
+++ config/routes.rb
@@ -17,6 +17,8 @@ Rails.application.routes.draw do
 
   resources :comments, except: [:new, :create]
 
+  resources :users, only: [:index, :show]
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
