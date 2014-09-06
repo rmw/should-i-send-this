e85269dfe6b07f2311b72dbf1b2add219df1939e
Avi Fox-Rosen <frosenox@gmail.com>
2014-09-06 17:54:07 -0400

    profile link in nav

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -14,6 +14,11 @@ class CommentsController < ApplicationController
     redirect_to document_path(version.document)
   end
 
+  def new
+    @version = Version.find(params[:id])
+    @comment = Comment.new
+  end
+
   private
     def comment_params
       params.require(:comment).permit(:body)
