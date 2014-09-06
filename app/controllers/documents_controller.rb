e0980a1ae10300dec4124f485509a227fd474579
Avi Fox-Rosen <frosenox@gmail.com>
2014-09-06 16:03:15 -0400

    undo commenting in controller

--- app/controllers/documents_controller.rb
+++ app/controllers/documents_controller.rb
@@ -1,5 +1,5 @@
 class DocumentsController < ApplicationController
-  # before_action :authenticate_user!
+  before_action :authenticate_user!
 
   def index
     @documents = Document.where(privacy: false).order(created_at: :desc)
