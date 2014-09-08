455e3e98156732337bbcddb301fba80a6b5e9435
Avi Fox-Rosen & Yariv Kirschner <pair+avifoxi.ykirschner@devbootcamp.com>
2014-09-08 12:35:07 -0400

    add email parser in model concerns, for parsing params from mail gun

--- app/controllers/email_controller.rb
+++ app/controllers/email_controller.rb
@@ -3,8 +3,9 @@ class EmailController < ApplicationController
   skip_before_filter :verify_authenticity_token, :only => [:create]
 
   def create
-    @params = params
-    UserMailer.test_email(@params).deliver
+    sender = EmailParser.sender(params)
+    content = EmailParser.content(params)
+    UserMailer.test_email(sender, content)
     redirect_to root_url
   end
 end
