8097a6a53b31fdca95db5885302f0d96141abdce
Avi Fox-Rosen & Yariv Kirschner <pair+avifoxi.ykirschner@devbootcamp.com>
2014-09-08 09:53:37 -0400

    disable authentication token in email controller

--- app/controllers/email_controller.rb
+++ app/controllers/email_controller.rb
@@ -1,5 +1,7 @@
 class EmailController < ApplicationController
 
+  skip_before_filter :verify_authenticity_token, :only => [:create]
+
   def create
     @params = params
     UserMailer.test_email(@params).deliver
