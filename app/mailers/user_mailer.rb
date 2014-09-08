526e689c529856ad71f74c3ca89955f824d09854
Alan Cohen <awc583@gmail.com>
2014-09-08 15:54:34 -0400

    added error checking to sentiment

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -5,20 +5,26 @@ class UserMailer < ActionMailer::Base
   def email(sender, content)
     alchemist = AlchemyData.new(content)
 
-    case alchemist.sentiment
-    when -1..0
-      response_body << "Are you intending to send a negative message? If so send away!"
-    when 0..1
-      response_body << "Looks good to me. Send away!"
+    if alchemist
+      sentiment = alchemist.sentiment
     else
-      response_body << "Should I Send This is currently under maintenance."
+      sentiment = 20
     end
+
+    # case alchemist.sentiment
+    # when -1..0
+    #   response_body << "Are you intending to send a negative message? If so send away!"
+    # when 0..1
+    #   response_body << "Looks good to me. Send away!"
+    # else
+    #   response_body << "Should I Send This is currently under maintenance."
+    # end
 
     # content = content
     # @url  = 'http://example.com/login'
     # @params = params
     mail(to: sender,
          subject: 'Your Analyses From... Should I Send This?',
-         body: response_body)
+         body: sentiment)
   end
 end
